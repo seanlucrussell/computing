5eee5a1ccc6f1cc68f8b9054364baf2e5fe88db1
seanlucrussell <seanlucrussell@gmail.com>
2018-11-21 16:39:51 -0700

    updated generator

--- Python/Simulation/DungeonGenerator.py
+++ Python/Simulation/DungeonGenerator.py
@@ -10,74 +10,40 @@ scale = 15
 
 def out_of_bounds(x,y):
     return x < 0 or x >= width or y < 0 or y >= height
-def generate_caves_v2(player_x,player_y):
-    caves = numpy.zeros((height,width),dtype=int)
-    caves[player_x,player_y] = 2
-    update_caves(player_x,player_y,caves)
+def set_square(x,y,value,caves):
+    if out_of_bounds(x,y): return
+    caves[x,y] = value
+    for square in [(x-1,y-1),(x-1,y),(x-1,y+1),(x,y+1),(x+1,y+1),(x+1,y),(x+1,y-1),(x,y-1)]:
+        avoid_corners(*square,caves)
+def generate_caves(x,y):
+    caves = create_data()
+    caves[x,y] = 2
+    update_caves(x,y,caves)
     return caves
 def adjacent_squares(x,y):
-    adjacent = [(x-1,y), (x+1,y), (x,y-1), (x,y+1)]
-    for square in adjacent:
-        if not out_of_bounds(*square):
-            yield square
-def update_square(i,j):
-    if caves[i,j] != 0:
-        return
-    directions = [(-1,-1),(-1,0),(-1,1),(0,1),(1,1),(1,0),(1,-1),(0,-1)]
-    adjacent = [(i+x[0],j+x[1]) for x in directions]
+    return (s for s in [(x-1,y), (x+1,y), (x,y-1), (x,y+1)] if not out_of_bounds(*s))
+def avoid_corners(x,y,caves):
+    if out_of_bounds(x,y) or caves[x,y] != 0: return
+    adj = [(x-1,y-1),(x-1,y),(x-1,y+1),(x,y+1),(x+1,y+1),(x+1,y),(x+1,y-1),(x,y-1)]
     for n in range(0,8,2):
-        if out_of_bounds(*adjacent[n]):
-            continue
-        one = caves[adjacent[(n-1)%8]]
-        two = caves[adjacent[n]]
-        three = caves[adjacent[(n+1)%8]]
-        if one == three and one != two and one != 0 and two != 0:
-            caves[i,j] = one
+        if out_of_bounds(*adj[n]): continue
+        side1,side2,corner = caves[adj[(n-1)%8]],caves[adj[(n+1)%8]],caves[adj[n]]
+        if side1 == side2 and side1 != corner and side1 != 0 and corner != 0:
+            set_square(x,y,side1,caves)
             return
-def update_corner(i,j):
-    one = caves[i,j]
-    two = caves[i+1,j]
-    three = caves[i,j+1]
-    four = caves[i+1,j+1]
-    if one == four:
-        if two == 0 and three != 0 and three != one:
-            caves[i+1,j] = one
-        elif two != 0 and two != one and three == 0:
-            caves[i,j+1] = one
-    elif two == three:
-        if one == 0 and four != 0 and four != two:
-            caves[i,j] = two
-        elif one != 0 and one != two and four == 0:
-            caves[i+1,j+1] = two
-def update_corners():
-    for i in range(width):
-        for j in range(height):
-            update_square(i,j)
-    '''
-    for i in range(width-1):
-        for j in range(height-1):
-            update_corner(i,j)
-    '''
-def successors(x,y):
-    for cell in adjacent_squares(x,y):
-        square = caves[x,y]
-        if square == 0 or square == 2:
-            yield cell
-def update_caves(player_x,player_y,caves):
-    frontier = [(player_x,player_y)]
-    depth = 10
-    for i in range(depth):
+def update_caves(x,y,caves):
+    frontier, explored = [(x,y)], set()
+    for i in range(100):
         next_frontier = []
         while frontier:
             exploring = frontier.pop(0)
-            for successor in successors(*exploring):
-                update_square(*successor)
-                if  caves[successor] == 0:
-                    caves[successor] = 1 if random.random() < .45 else 2
-                if caves[successor] == 2:
-                    next_frontier.append(successor)
+            explored.add(exploring)
+            for successor in adjacent_squares(*exploring):
+                if successor in explored: continue
+                if  caves[successor] == 0: set_square(*successor, random.randint(1,2), caves)
+                if caves[successor] == 2: next_frontier.append(successor)
         frontier = next_frontier
-    update_corners()
+        if not frontier: break
 
 def create_window():
     main = tkinter.Tk()
@@ -86,41 +52,29 @@ def create_window():
     canvas = tkinter.Canvas(main, width=width*scale, height=height*scale)
     canvas.pack()
     return main,canvas
-
+def create_data():
+    return numpy.zeros((height,width),dtype=int)
 def display_data(canvas,data):
     px=numpy.repeat(numpy.repeat(data*80,scale,axis=1),scale,axis=0)
     im=Image.frombytes('L', (width*scale,height*scale), px.astype('b').tostring())
     canvas.photo = ImageTk.PhotoImage(image=im)
     canvas.create_image(0,0,image=canvas.photo,anchor=tkinter.NW)
-
 def key(event):
-    """shows key or tk code for the key"""
     global move
     key = event.keysym
-    if key == 'Escape':
-        main.destroy()
-    elif key == 'Up' or key == 'Down' or key == 'Left' or key == 'Right':
-        move = key
-
+    if key == 'Up' or key == 'Down' or key == 'Left' or key == 'Right': move = key
 def move_player(move,x,y,caves):
-    if move == 'Left' and not out_of_bounds(x,y-1) and caves[x,y-1] == 2:
-        return x,y-1
-    elif move == 'Right' and not out_of_bounds(x,y+1) and caves[x,y+1] == 2:
-        return x,y+1
-    elif move == 'Up' and not out_of_bounds(x-1,y) and caves[x-1,y] == 2:
-        return x-1,y
-    elif move == 'Down' and not out_of_bounds(x+1,y) and caves[x+1,y] == 2:
-        return x+1,y
-    else:
-        return x,y
+    if move == 'Left' and not out_of_bounds(x,y-1) and caves[x,y-1] == 2: return x,y-1
+    elif move == 'Right' and not out_of_bounds(x,y+1) and caves[x,y+1] == 2: return x,y+1
+    elif move == 'Up' and not out_of_bounds(x-1,y) and caves[x-1,y] == 2: return x-1,y
+    elif move == 'Down' and not out_of_bounds(x+1,y) and caves[x+1,y] == 2: return x+1,y
+    else: return x,y
 
 main,canvas = create_window()    
 main.bind_all('<Key>', key)
 move = None
-x = width//2
-y = height//2
-caves = generate_caves_v2(x,y)
-
+x,y = width//2, height//2
+caves = generate_caves(x,y)
 while True:
     update_caves(x,y,caves)
     data = numpy.copy(caves)
@@ -132,10 +86,3 @@ while True:
     move = None
 
 main.mainloop()
-
-y = 1/(1+e^-x)
-1+e^-x = 1/y
-e^-x = 1/y - 1
-ln(e^-x) = ln(1/y - 1)
--x = ln(1/y - 1)
-x = -ln(1/y - 1)
