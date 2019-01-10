1273f65311ee8ad2b70331483a4c65188462ecad
seanlucrussell <seanlucrussell@gmail.com>
2019-01-09 21:47:26 -0700

    new batch of changes

--- machineLearning/variational_autoencoder.py
+++ machineLearning/variational_autoencoder.py
@@ -3,58 +3,48 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.losses import binary_crossentropy
 from tensorflow.keras import backend as K
-
+import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
 
-def sampling(args):
-    z_mean, z_log_var = args
-    batch = K.shape(z_mean)[0]
-    dim = K.int_shape(z_mean)[1]
-    epsilon = K.random_normal(shape=(batch, dim))
-    return z_mean + K.exp(0.5 * z_log_var) * epsilon
 # MNIST dataset
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-image_size = x_train.shape[1]
-original_dim = image_size * image_size
+(x_train, _), (x_test, _) = mnist.load_data()
+original_dim = x_train.shape[1] * x_train.shape[2]
 x_train = np.reshape(x_train, [-1, original_dim])
 x_test = np.reshape(x_test, [-1, original_dim])
 x_train = x_train.astype('float32') / 255
 x_test = x_test.astype('float32') / 255
-# network parameters
-input_shape = (original_dim, )
-intermediate_dim = 512
-batch_size = 128
-latent_dim = 4
-epochs = 50
 
-# build encoder model
-inputs = Input(shape=input_shape, name='encoder_input')
-x = Dense(intermediate_dim, activation='relu')(inputs)
-z_mean = Dense(latent_dim, name='z_mean')(x)
-z_log_var = Dense(latent_dim, name='z_log_var')(x)
-# use reparameterization trick to push the sampling out as input
-# note that "output_shape" isn't necessary with the TensorFlow backend
-z = Lambda(sampling, output_shape=(latent_dim,), name='z')([z_mean, z_log_var])
-# instantiate encoder model
+# network parameters
+latent_dim = 4
+hidden_layer_size = 512
+batch_size = 256
+epochs = 1
+beta = 1.0
+
+# ENCODER
+inputs = Input(shape=(original_dim,), name='encoder_input')
+h1 = Dense(hidden_layer_size, activation='relu')(inputs)
+z_mean = Dense(latent_dim, name='z_mean')(h1)
+z_log_var = Dense(latent_dim, name='z_log_var')(h1)
+def sampling(args):
+    # use reparameterization trick to push the sampling out as input
+    z_mean, z_log_var = args
+    return z_mean + K.exp(0.5 * z_log_var) * K.random_normal(shape=K.shape(z_mean))
+z = Lambda(sampling, name='z')([z_mean, z_log_var])
 encoder = Model(inputs, [z_mean, z_log_var, z], name='encoder')
-# build decoder model
+# DECODER
 latent_inputs = Input(shape=(latent_dim,), name='z_sampling')
-x = Dense(intermediate_dim, activation='relu')(latent_inputs)
-outputs = Dense(original_dim, activation='sigmoid')(x)
-# instantiate decoder model
+h2 = Dense(hidden_layer_size, activation='relu')(latent_inputs)
+outputs = Dense(original_dim, activation='sigmoid')(h2)
 decoder = Model(latent_inputs, outputs, name='decoder')
-# instantiate VAE model
+# FULL VARTIATIONAL AUTOENCODER
 outputs = decoder(encoder(inputs)[2])
 vae = Model(inputs, outputs, name='vae_mlp')
-models = (encoder, decoder)
-data = (x_test, y_test)
-# VAE loss = crossentropy_loss + kl_loss
-reconstruction_loss = binary_crossentropy(inputs,outputs)
-reconstruction_loss *= original_dim
-kl_loss = 1 + z_log_var - K.square(z_mean) - K.exp(z_log_var)
+# VAE loss = reconstruction_loss + kl_loss
+reconstruction_loss = binary_crossentropy(inputs,outputs) * original_dim
+kl_loss = 0.5 * beta * (K.square(z_mean) + K.exp(z_log_var) - 1 - z_log_var)
 kl_loss = K.sum(kl_loss, axis=-1)
-kl_loss *= -0.5
 vae_loss = K.mean(reconstruction_loss + kl_loss)
 vae.add_loss(vae_loss)
 vae.compile(optimizer='adam')
@@ -67,13 +57,21 @@ except:
             validation_data=(x_test, None))
     vae.save_weights('vae_mnist.h5')
 
-start = 0.0
+'''
+create sliders and interactive charts. Automatically creates
+and positions (not well) sliders based off of number of latent
+variables specified in the hyperparameters.
+'''
 fig, ax = plt.subplots()
-start_encoding = np.full((1,latent_dim),start)
-im = plt.imshow(decoder.predict(start_encoding).reshape(28,28),cmap='Greys')
-im.set_clim([0,1])
 plt.axis('off')
+# initial value of all the latent variables (and sliders)
+start = 0.0
+start_encoding = np.full((1,latent_dim),start)
+# display reconstruced image
+image = decoder.predict(start_encoding).reshape(28,28)
+im = plt.imshow(image,cmap='Greys', vmin=0, vmax=1)
 
+# update is called whenever the value of a slider changes
 def update(val):
     encoding = []
     for slider in sliders:
@@ -83,6 +81,7 @@ def update(val):
     im.set_data(data)
     fig.canvas.draw_idle()
 
+#this bit creates the sliders and positions them
 sliders = []
 for i in range(latent_dim):
     axcolor = plt.axes([0.05, 0.03 * i, 0.2, 0.025])
@@ -91,4 +90,3 @@ for i in range(latent_dim):
     sliders.append(slider)
 
 plt.show()
-
---
+++ web/website/top.js
@@ -0,0 +1,12 @@
+document.write('\
+  <head>\
+    <link rel="stylesheet" type="text/css" href="style.css">\
+  </head>\
+  <body>\
+    <div class="navbar">\
+      <a class="navlink" href="#home">HOME</a>\
+      <a class="navlink" href="about.html">ABOUT</a>\
+      <a class="navlink" href="#contact">CONTACT</a>\
+    </div>\
+    <div class="contents">\
+');
